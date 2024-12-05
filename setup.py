e9ee3b156eec9a513bbae303ad6255854f6e6902
Taymullah <sm.taymullah@gmail.com>
2024-12-05 01:30:13 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,45 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Sales"
+AUTHOR_USER_NAME = "Taymullah"
+SRC_REPO = "Sales"
+AUTHOR_EMAIL = "sm.taymullah@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for machine learning app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
+
+install_requires = (requirements,)  # Automatically pulls from requirements.txt
+classifiers = (
+    [
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
+
+python_requires = ">=3.10"
